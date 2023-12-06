b8501f3a990fffae05354fc37767e01f0410cbec
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-12-06 22:48:51 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
+import math
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+
+
+happy_new_year()
+print('Happy New Year!')
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+     int_list = [math.pow(int, 2) for int in int_list]
+     print(int_list)
+square_integers([1,2,3,4,5])
+def fizzbuzz(num):
+    if (num % 3 == 0 and num % 5 == 0):
+     print("FizzBuzz")
+    elif(num % 3 == 0):
+     print("Fizz")
+    elif(num % 5 == 0) :
+     print("Buzz")
+    else:
+     print(num)         
+fizzbuzz(55)
